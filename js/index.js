3367af0bf7e5ec4042ff9429cbe5b083aa86f3cf
SRAKIB17 <rakibulssc5@gmail.com>
2022-02-20 20:01:13 +0600

    update index js

--- js/index.js
+++ js/index.js
@@ -4,37 +4,56 @@ const Header = getId('header');
 const leftArrayTitle = ['Jumping another page','Result convert cgp','Simple calculator','Temperature','Icon manager','BMI calculator']
 leftArrayTitle.sort();
 const left = getId('left');
-// left.style.height = window.innerHeight-Header.clientHeight+'px';
+
 // create ul 
 const leftUl = document.createElement('ul')
 
 left.appendChild(leftUl)
 for (let index = 0; index < leftArrayTitle.length; index++) {
     const li = document.createElement('li')
-    li.setAttribute('onclick','goPage(this)')
-    li.innerText = leftArrayTitle[index];
-
+    
+    const a = document.createElement('a');
+    // here first: target page second:prev path 
+    a.setAttribute('href',`#${leftArrayTitle[index]}#services`)
+    a.setAttribute('onclick','goPage(this)')
+    a.innerText = leftArrayTitle[index];
+    li.appendChild(a);
     leftUl.appendChild(li)
 }
 
 
-function goPage(link){
-    localStorage.setItem('url-page','services/'+link.innerText);
-    titleMake()
+function goPage(page){
+    const getLink = page.href
+    
+    const splitLink = getLink.split('#');
+
+    const link = `/${splitLink[2]}/${splitLink[1]}`;
+    console.log(link)
+    getId('pageTitle').innerText = decodeURI(splitLink[1]);
+    // location.assign(link)
+    localStorage.setItem('url-page',link);
     getId('result').style.display = 'none'
     getId('search-input').style.display = 'none'
-    getId('htmlPage').src = localStorage.getItem('url-page');
+    document.getElementById('htmlPage').src = localStorage.getItem('url-page');
 }
+
+
+
+
+// function goPage(link){
+//     localStorage.setItem('url-page','services/'+link.innerText);
+//     titleMake()
+//     getId('result').style.display = 'none'
+//     getId('search-input').style.display = 'none'
+//     getId('htmlPage').src = localStorage.getItem('url-page');
+// }
 // function for title 
 titleMake()
 function titleMake(){
-    const title = localStorage.getItem('url-page').split('/');
-    if (title.length == 2){
-        getId('pageTitle').innerText = title[1]
-    }
-    else{
-        getId('pageTitle').innerText = title[0].split('.')[0];
-    }
+    const titleUrl = localStorage.getItem('url-page').split('/');
+    const title = titleUrl[titleUrl.length-1];
+    console.log(title)
+    getId('pageTitle').innerText = decodeURI(title);
 }
 
 
