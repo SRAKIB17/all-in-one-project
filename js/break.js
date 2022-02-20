ad247c997eb6eec98a7c828cb31236e7a9cc58e7
SRAKIB17 <rakibulssc5@gmail.com>
2022-02-20 22:23:54 +0600

    ok responce

--- js/break.js
+++ js/break.js
@@ -1,46 +1,14 @@
-const breakPoint = window.innerWidth;
 
 const center = getId('center')
 const footerMenu = getId('footerMenu')
-const showFooter = getId('showFooter');
-const footerSection = getId('footerSection')
-if(breakPoint > 990){
-    footerMenu.style.height = window.innerHeight-Header.clientHeight+'px';
-    showFooter.style.display = 'none'
 
-    // for center positon 
-    center.style.height = window.innerHeight-Header.clientHeight+'px';
-}
-else if(breakPoint < 990){
-    
-    footerSection.style.marginTop = '30px'
-    showFooter.style.top = Header.clientHeight+10+'px';
-    footerMenu.style.top = Header.clientHeight+10+'px';
-    footerMenu.style.height = window.innerHeight-Header.clientHeight+'px';
-    footerMenu.style.width = '150px'
-    // for center positon 
+const iframe = getId('htmlPage')
 
-    center.style.height = window.innerHeight-Header.clientHeight+'px';
-}
-
+reload()
 function reload(){
-    if(breakPoint > 990){
-        footerMenu.style.height = window.innerHeight-Header.clientHeight+'px';
-        showFooter.style.display = 'none'
-        getId('leftMenuBar').style.display = 'none';
-        // for center positon 
-        center.style.height = window.innerHeight-Header.clientHeight+'px';
-    }
-    else if(breakPoint < 990){
+        iframe.style.height = window.innerHeight-Header.clientHeight+'px';
         
-        footerSection.style.marginTop = '30px'
-        showFooter.style.top = Header.clientHeight+10+'px';
-    
-        footerMenu.style.top = Header.clientHeight+10+'px';
         footerMenu.style.height = window.innerHeight-Header.clientHeight+'px';
-        footerMenu.style.width = '150px'
-        // for center positon 
-    
+
         center.style.height = window.innerHeight-Header.clientHeight+'px';
-    }
 }
